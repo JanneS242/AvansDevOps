b256c5f0a080707f11e0b5b5d0263c0fab6e1208
Janne Sterk <jannesterk@hotmail.com>
2023-03-29 20:48:20 +0200

    end date check

--- src/sprint/releaseSprint.ts
+++ src/sprint/releaseSprint.ts
@@ -6,25 +6,35 @@ export class ReleaseSprint extends Sprint{
     resultsGood : boolean = false;
     cancelled : boolean = !this.resultsGood;
         
+    successfullSprint(){
+        this.resultsGood = true;
+    }
     
     public startPipeline(): void {
-        if(this.cancelled){
-            console.log("Release sprint is cancelled");
-            //notificatie naar SM
-            this.teamMembers.forEach(member =>{
-                if(member instanceof ScrumMaster){
-                    member.notificationTypes.forEach(type => {
-                        type.notify();
-                    });
-                }
-            });
+        if(this.endDate < new Date()){
+            this.finished = true;
+            console.log("End date has already expired");
         } else{
-            console.log("Starting pipeline for release sprint");
-            
-            if(this.pipeline != null){
-                const executeVisitor = new ExecuteVisitor();
-                this.pipeline.accept(executeVisitor);
+            if(this.cancelled){
+                console.log("Release sprint is cancelled");
+                //notificatie naar SM
+                this.teamMembers.forEach(member =>{
+                    if(member instanceof ScrumMaster){
+                        member.notificationTypes.forEach(type => {
+                            type.notify();
+                        });
+                    }
+                });
+            } else{
+                console.log("Starting pipeline for release sprint");
+                
+                if(this.pipeline != null){
+                    const executeVisitor = new ExecuteVisitor();
+                    this.pipeline.accept(executeVisitor);
+                }
             }
         }
+
+        
     }
 }
--- src/sprint/reviewSprint.ts
+++ src/sprint/reviewSprint.ts
@@ -9,15 +9,20 @@ export class ReviewSprint extends Sprint{
     }
     
     public startPipeline(): void {
-        if(this.summaryUploaded){
-            console.log("Starting pipeline for reviewSprint");
-            
-            if(this.pipeline != null){
-                const executeVisitor = new ExecuteVisitor();
-                this.pipeline.accept(executeVisitor);
+        if(this.endDate < new Date()){
+            this.finished = true;
+            console.log("End date has already expired");
+        } else{
+            if(this.summaryUploaded){
+                console.log("Starting pipeline for reviewSprint");
+                
+                if(this.pipeline != null){
+                    const executeVisitor = new ExecuteVisitor();
+                    this.pipeline.accept(executeVisitor);
+                }
+            } else {
+                console.log("Cannot start pipeline because there is no summaryUploaded")
             }
-        } else {
-            console.log("Cannot start pipeline because there is no summaryUploaded")
         }
     }    
 
