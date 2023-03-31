58749c26313a3d55283ad1d99c66d4a1ce0cb530
Janne Sterk <jannesterk@hotmail.com>
2023-03-31 09:34:23 +0200

    removed setters from reportComponent

--- src/report/reportComponent.ts
+++ src/report/reportComponent.ts
@@ -1,31 +1,8 @@
 export class ReportComponent{
 
-    constructor(public logo : string, public projectName : string, public businessName : string, public version : number, public date : Date){
-
-    }
+    constructor(public logo : string, public projectName : string, public businessName : string, public version : number, public date : Date){}
 
     public toString() : string{
         return `${this.logo} ${this.businessName} \n projectName: ${this.projectName}, version: ${this.version}, date: ${this.date}`;
     }
-
-    public setLogo(logo : string){
-        this.logo = logo;
-    }
-
-    public setProjectName(name : string){
-        this.projectName = name;
-    }
-
-    public setBusinessName(name : string){
-        this.businessName = name;
-    }
-
-    public setVersion(version : number){
-        this.version = version;
-    }
-
-    public setDate(date : Date){
-        this.date = date;
-    }
-
 }
